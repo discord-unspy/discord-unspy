de3463bf19c8f8d7650688bdad6fc51301610efe
KorbsStudio <51213244+KorbsStudio@users.noreply.github.com>
2022-04-16 21:39:28 -0400

    Add OS Detection for titlebar

--- src/titlebar.js
+++ src/titlebar.js
@@ -17,3 +17,7 @@ function titlebar(params) {
       document.body.className = 'none';
   }
 };
+
+var htmlElement=document.documentElement;if(navigator.platform.match(/(Mac)/i)){htmlElement.className='Mac'}
+if(navigator.platform.match(/(Linux)/i)){htmlElement.className='Linux'}else{htmlElement.className='Windows'}
+document.addEventListener('click',function(event){var target=event.target;if(target.getAttribute&&target.getAttribute('data-action')==='switch-os'){event.preventDefault();htmlElement.className=target.getAttribute('data-os')}})
