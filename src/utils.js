7919f2e995b616af77c3249464f2d3b61d1fa737
Ashley <iamashley@tuta.io>
2022-05-04 00:17:27 +0300

    format the file lol
    my name is joe biden

--- src/utils.js
+++ src/utils.js
@@ -4,71 +4,72 @@
  * inculudes;
  * https://github.com/mrdoob/eventdispatcher.js/ (SPDX-License-Identifier: MIT)
  */
-    class EventDispatcher {
-		addEventListener(type, listener) {
-			if (this._listeners === undefined) this._listeners = {};
-			const listeners = this._listeners;
+class EventDispatcher {
+  addEventListener(type, listener) {
+    if (this._listeners === undefined) this._listeners = {};
+    const listeners = this._listeners;
 
-			if (listeners[type] === undefined) {
-				listeners[type] = [];
-			}
+    if (listeners[type] === undefined) {
+      listeners[type] = [];
+    }
 
-			if (listeners[type].indexOf(listener) === -1) {
-				listeners[type].push(listener);
-			}
-		}
+    if (listeners[type].indexOf(listener) === -1) {
+      listeners[type].push(listener);
+    }
+  }
 
-		hasEventListener(type, listener) {
-			if (this._listeners === undefined) return false;
-			const listeners = this._listeners;
-			return listeners[type] !== undefined && listeners[type].indexOf(listener) !== -1;
-		}
+  hasEventListener(type, listener) {
+    if (this._listeners === undefined) return false;
+    const listeners = this._listeners;
+    return (
+      listeners[type] !== undefined && listeners[type].indexOf(listener) !== -1
+    );
+  }
 
-		removeEventListener(type, listener) {
-			if (this._listeners === undefined) return;
-			const listeners = this._listeners;
-			const listenerArray = listeners[type];
+  removeEventListener(type, listener) {
+    if (this._listeners === undefined) return;
+    const listeners = this._listeners;
+    const listenerArray = listeners[type];
 
-			if (listenerArray !== undefined) {
-				const index = listenerArray.indexOf(listener);
+    if (listenerArray !== undefined) {
+      const index = listenerArray.indexOf(listener);
 
-				if (index !== -1) {
-					listenerArray.splice(index, 1);
-				}
-			}
-		}
+      if (index !== -1) {
+        listenerArray.splice(index, 1);
+      }
+    }
+  }
 
-		dispatchEvent(event) {
-			if (this._listeners === undefined) return;
-			const listeners = this._listeners;
-			const listenerArray = listeners[event.type];
+  dispatchEvent(event) {
+    if (this._listeners === undefined) return;
+    const listeners = this._listeners;
+    const listenerArray = listeners[event.type];
 
-			if (listenerArray !== undefined) {
-				event.target = this; // Make a copy, in case listeners are removed while iterating.
+    if (listenerArray !== undefined) {
+      event.target = this; // Make a copy, in case listeners are removed while iterating.
 
-				const array = listenerArray.slice(0);
+      const array = listenerArray.slice(0);
 
-				for (let i = 0, l = array.length; i < l; i++) {
-					array[i].call(this, event);
-				}
+      for (let i = 0, l = array.length; i < l; i++) {
+        array[i].call(this, event);
+      }
 
-				event.target = null;
-			}
-		}
-
-	}
+      event.target = null;
+    }
+  }
+}
 /* Unspy utils - console API
-     * usage:console.api
-    */
-  if (typeof console._commandLineAPI !== 'undefined') {
-    console.API = console._commandLineAPI;
-   } else if (typeof console._inspectorCommandLineAPI !== 'undefined') {
-    console.API = console._inspectorCommandLineAPI;
-   } else if (typeof console.clear !== 'undefined') {
-    console.API = console;
- }
- /* Unspy utils - Console warning
-  * warns the user
-*/
-  console.log("%cNya~","color: pink; font-size: 100px");
-  console.log("%cHey Qtpie please dont copy or paste anything here~ :3  ","color: purple; font-size: 30px")
+ * usage:console.API
+ */
+if (typeof console._commandLineAPI !== 'undefined') {
+  console.API = console._commandLineAPI;
+} else if (typeof console._inspectorCommandLineAPI !== 'undefined') {
+  console.API = console._inspectorCommandLineAPI;
+} else if (typeof console.clear !== 'undefined') {
+  console.API = console;
+}
+/* Unspy utils - Console warning
+ * warns the user
+ */
+console.log('%cNya~', 'color: pink; font-size: 100px');
+console.log(  '%cHey Qtpie please dont copy or paste anything here~ :3  ','color: purple; font-size: 30px');
